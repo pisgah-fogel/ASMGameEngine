32d8675514c70f3ed657dd97d3976f5e07c3f510
Phileas Fogg <phileas.fogg@mail.com>
2020-09-16 23:32:49 +0200

    WIP: C Proof Of Concept

---
+++ POC/main.c
@@ -0,0 +1,49 @@
+#include <SDL2/SDL.h> 
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+  
+int main() 
+{ 
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) { 
+        printf("Error initializing SDL: %s\n", SDL_GetError()); 
+    } 
+    SDL_Window* win = SDL_CreateWindow("GAME", 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        1000, 1000, 0);
+
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED); 
+    SDL_Surface* surface = IMG_Load("hand_open.png");
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_FreeSurface(surface);
+    SDL_Rect dest;
+    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+    dest.w *= 3; 
+    dest.h *= 3; 
+    dest.x = (1000 - dest.w) / 2;
+    dest.y = (1000 - dest.h) / 2;
+
+    int close = 0;
+    int speed = 300;
+
+
+    while (!close) {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    close = 1;
+                    break;
+            }
+        }
+
+        SDL_RenderClear(rend);
+        SDL_RenderCopy(rend, tex, NULL, &dest);
+        SDL_RenderPresent(rend);
+        SDL_Delay(1000 / 60);
+    }
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(win);
+    return 0; 
+} 
---
+++ debian_ubuntu_install_dependencies.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install libsdl2-dev
+sudo apt install libsdl2-image-dev
