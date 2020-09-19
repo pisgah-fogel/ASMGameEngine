49ddbae7290b8d02d945a729b5d4122da2c69b65
DETREZ Aubin <aubin.detrez@groupe-esigelec.org>
2020-09-19 23:57:05 +0200

    Moving Sprite to Sprite.h

--- CVersion/main.c
+++ CVersion/main.c
@@ -4,6 +4,8 @@
 #include <SDL2/SDL_timer.h>
 #include <chipmunk/chipmunk.h>
 
+#include "Sprite.h"
+
 #define ERROR_HANDLING
 
 #define WIN_X 800
@@ -35,84 +37,11 @@ void freeSDL()
     SDL_DestroyWindow(win);
 }
 
-struct Sprite
-{
-    SDL_Texture* texture;
-    SDL_Rect rect; // x/y/w/h destination position and scale
-    SDL_Rect clip; // Texture clipping
-    SDL_Point center; // Rotation center
-};
-
-struct Sprite* initSprite()
-{
-    SDL_Surface* surface = IMG_Load("test1.png");
-#ifdef ERROR_HANDLING
-    if (surface == NULL) {
-        fprintf(stderr, "initSprite: IMG_Load failed: %s\n", SDL_GetError());
-        return NULL;
-    }
-#endif
-
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
-
-    SDL_FreeSurface(surface);
-#ifdef ERROR_HANDLING
-    if (tex == NULL) {
-        fprintf(stderr, "initSprite: CreateTextureFromSurface failed: %s\n", SDL_GetError());
-        return NULL;
-    }
-#endif
-    struct Sprite* output = malloc(sizeof(struct Sprite));
-#ifdef ERROR_HANDLING
-    if (output == NULL) {
-        fprintf(stderr, "initSprite: Malloc failed\n");
-        SDL_DestroyTexture(tex);
-        return NULL;
-    }
-#endif
-
-    SDL_QueryTexture(tex, NULL, NULL, &output->rect.w, &output->rect.h);
-    output->texture = tex;
-    output->rect = (struct SDL_Rect){200, 100, 100, 100};
-    output->clip = (struct SDL_Rect){0, 0, 100, 100};
-    output->center = (struct SDL_Point){50, 50};
-    return output;
-}
-
-void renderSprite(SDL_Renderer* renderer, struct Sprite* ptr) {
-#ifdef ERROR_HANDLING
-    if (ptr == NULL || ptr->texture == NULL || renderer == NULL) {
-        return;
-    }
-#endif
-    SDL_RenderCopy(renderer, ptr->texture, &ptr->clip, &ptr->rect);
-    //SDL_RenderCopyEx(rend, tex, NULL, &dest, real_to_sprite_rot(rot), &centerImg, SDL_FLIP_NONE);
-}
-
-void freeSprite(struct Sprite* ptr)
-{
-#ifdef ERROR_HANDLING
-    if (ptr == NULL) {
-        fprintf(stderr, "Sprite's pointer is not valid\n");
-        return;
-    }
-    else if (ptr->texture == NULL) {
-        fprintf(stderr, "Texture's pointer is not valid (but Sprite's pointer is)\n");
-        free(ptr);
-        ptr = NULL;
-        return;
-    }
-#endif
-    SDL_DestroyTexture(ptr->texture);
-    free(ptr);
-    ptr = NULL;
-}
-
 int main(int argc, char **argv)
 {
     initSDL();
 
-    struct Sprite* mSprite = initSprite();
+    struct Sprite* mSprite = initSprite(rend);
 
     int close = 0;
     while (!close) {
