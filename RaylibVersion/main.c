748b0f140ff01ebdea77a3c674163226dbda578c
DETREZ Aubin <aubin.detrez@groupe-esigelec.org>
2020-09-20 08:25:25 +0200

    Correct raylibmakefile

--- RaylibVersion/main.c
+++ RaylibVersion/main.c
@@ -1,45 +1,51 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic 3d example
-*
-*   Welcome to raylib!
-*
-*   To compile example, just press F5.
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2020 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+#define PHYSAC_IMPLEMENTATION
+#define PHYSAC_NO_THREADS
+#include "physac.h"
 
-int main() 
+int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib");
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(screenWidth, screenHeight, "Physac [raylib] - Physics friction");
 
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.type = CAMERA_PERSPECTIVE;
-    
-    SetCameraMode(camera, CAMERA_ORBITAL);
+    // Physac logo drawing position
+    int logoX = screenWidth - MeasureText("Physac", 30) - 10;
+    int logoY = 15;
 
-    Vector3 cubePosition = { 0.0f };
+    // Initialize physics and default physics bodies
+    InitPhysics();
+
+    // Create floor rectangle physics body
+    PhysicsBody floor = CreatePhysicsBodyRectangle((Vector2){ screenWidth/2, screenHeight }, screenWidth, 100, 10);
+    floor->enabled = false; // Disable body state to convert it to static (no dynamics, but collisions)
+    PhysicsBody wall = CreatePhysicsBodyRectangle((Vector2){ screenWidth/2, screenHeight*0.8f }, 10, 80, 10);
+    wall->enabled = false; // Disable body state to convert it to static (no dynamics, but collisions)
+
+    // Create left ramp physics body
+    PhysicsBody rectLeft = CreatePhysicsBodyRectangle((Vector2){ 25, screenHeight - 5 }, 250, 250, 10);
+    rectLeft->enabled = false; // Disable body state to convert it to static (no dynamics, but collisions)
+    SetPhysicsBodyRotation(rectLeft, 30*DEG2RAD);
+
+    // Create right ramp  physics body
+    PhysicsBody rectRight = CreatePhysicsBodyRectangle((Vector2){ screenWidth - 25, screenHeight - 5 }, 250, 250, 10);
+    rectRight->enabled = false; // Disable body state to convert it to static (no dynamics, but collisions)
+    SetPhysicsBodyRotation(rectRight, 330*DEG2RAD);
+
+    // Create dynamic physics bodies
+    PhysicsBody bodyA = CreatePhysicsBodyRectangle((Vector2){ 35, screenHeight*0.6f }, 40, 40, 10);
+    bodyA->staticFriction = 0.1f;
+    bodyA->dynamicFriction = 0.1f;
+    SetPhysicsBodyRotation(bodyA, 30*DEG2RAD);
+
+    PhysicsBody bodyB = CreatePhysicsBodyRectangle((Vector2){ screenWidth - 35, screenHeight*0.6f }, 40, 40, 10);
+    bodyB->staticFriction = 1.0f;
+    bodyB->dynamicFriction = 1.0f;
+    SetPhysicsBodyRotation(bodyB, 330*DEG2RAD);
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -49,26 +55,64 @@ int main()
     {
         // Update
         //----------------------------------------------------------------------------------
-        UpdateCamera(&camera);
+        RunPhysicsStep();
+
+        if (IsKeyPressed('R'))    // Reset physics input
+        {
+            // Reset dynamic physics bodies position, velocity and rotation
+            bodyA->position = (Vector2){ 35, screenHeight*0.6f };
+            bodyA->velocity = (Vector2){ 0, 0 };
+            bodyA->angularVelocity = 0;
+            SetPhysicsBodyRotation(bodyA, 30*DEG2RAD);
+
+            bodyB->position = (Vector2){ screenWidth - 35, screenHeight*0.6f };
+            bodyB->velocity = (Vector2){ 0, 0 };
+            bodyB->angularVelocity = 0;
+            SetPhysicsBodyRotation(bodyB, 330*DEG2RAD);
+        }
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(BLACK);
 
-            BeginMode3D(camera);
+            DrawFPS(screenWidth - 90, screenHeight - 30);
 
-                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-                DrawGrid(10, 1.0f);
+            // Draw created physics bodies
+            int bodiesCount = GetPhysicsBodiesCount();
+            for (int i = 0; i < bodiesCount; i++)
+            {
+                PhysicsBody body = GetPhysicsBody(i);
 
-            EndMode3D();
+                if (body != NULL)
+                {
+                    int vertexCount = GetPhysicsShapeVerticesCount(i);
+                    for (int j = 0; j < vertexCount; j++)
+                    {
+                        // Get physics bodies shape vertices to draw lines
+                        // Note: GetPhysicsShapeVertex() already calculates rotation transformations
+                        Vector2 vertexA = GetPhysicsShapeVertex(body, j);
 
-            DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
+                        int jj = (((j + 1) < vertexCount) ? (j + 1) : 0);   // Get next vertex or first to close the shape
+                        Vector2 vertexB = GetPhysicsShapeVertex(body, jj);
 
-            DrawFPS(10, 10);
+                        DrawLineV(vertexA, vertexB, GREEN);     // Draw a line between two vertex positions
+                    }
+                }
+            }
+
+            DrawRectangle(0, screenHeight - 49, screenWidth, 49, BLACK);
+
+            DrawText("Friction amount", (screenWidth - MeasureText("Friction amount", 30))/2, 75, 30, WHITE);
+            DrawText("0.1", bodyA->position.x - MeasureText("0.1", 20)/2, bodyA->position.y - 7, 20, WHITE);
+            DrawText("1", bodyB->position.x - MeasureText("1", 20)/2, bodyB->position.y - 7, 20, WHITE);
+
+            DrawText("Press 'R' to reset example", 10, 10, 10, WHITE);
+
+            DrawText("Physac", logoX, logoY, 30, WHITE);
+            DrawText("Powered by", logoX + 50, logoY - 7, 10, WHITE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -76,6 +120,8 @@ int main()
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
+    ClosePhysics();       // Unitialize physics
+
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
